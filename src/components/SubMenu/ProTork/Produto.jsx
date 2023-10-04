6904961dda977123bba5d2cf1fc87bb977373413
santiagoidu <sr_santiago@icloud.com>
2023-10-04 17:11:30 -0300

    feat: Sub Menu Protork

--- src/components/SubMenu/ProTork/Produto.jsx
+++ src/components/SubMenu/ProTork/Produto.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuProTork">
+            <img src="https://i.ibb.co/Qc2yHdX/protork.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Bauleto" />
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
             
+            </div>
         </div>
     )
 }
