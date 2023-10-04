f7d76f869093c3fcd6f6e5ef1b76344d92d212e8
santiagoidu <sr_santiago@icloud.com>
2023-10-04 11:12:38 -0300

    fix: Photo logo Shark

--- src/components/SubMenu/SanMarino/Produto.jsx
+++ src/components/SubMenu/SanMarino/Produto.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuPeels">
+            <img src="https://i.ibb.co/v3Q1TRR/peels.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
             
+            </div>
         </div>
     )
 }
