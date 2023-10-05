27bf53ef4bc8d00a71f56526501c18c03a3cde45
santiagoidu <sr_santiago@icloud.com>
2023-10-05 13:28:12 -0300

    feat: Sub Menu MT

--- src/components/SubMenu/MT/Produto.jsx
+++ src/components/SubMenu/MT/Produto.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
-            
+    return (
+        <div className="MenuMt">
+            <img src="https://i.ibb.co/VHkyGgF/mt.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            </div>
         </div>
     )
 }
