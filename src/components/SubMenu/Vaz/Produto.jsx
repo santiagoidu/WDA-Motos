c6f0a90619609b39a248bbf5816ee8ca1d5ec10a
santiagoidu <sr_santiago@icloud.com>
2023-10-05 12:59:41 -0300

    feat: Sub Menu Vaz

--- src/components/SubMenu/Vaz/Produto.jsx
+++ src/components/SubMenu/Vaz/Produto.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
-            
+    return (
+        <div className="MenuVaz">
+            <img src="https://i.ibb.co/k3pr0cH/vaz.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            </div>
         </div>
     )
 }
