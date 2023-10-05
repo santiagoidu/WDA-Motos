c3d855e6874b0fe16389e2a4a403ec88b95be723
santiagoidu <sr_santiago@icloud.com>
2023-10-05 10:14:24 -0300

    feat: Sub Menu Shark

--- src/components/SubMenu/Shark/Produto.jsx
+++ src/components/SubMenu/Shark/Produto.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuShark">
+            <img src="https://i.ibb.co/PGsxFJK/shark.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            
+            </div>
         </div>
     )
 }
