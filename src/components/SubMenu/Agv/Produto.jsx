18afce895730e78069b6cf521450d3f5bbcee958
santiagoidu <sr_santiago@icloud.com>
2023-10-05 09:58:00 -0300

    feat: Sub Menu AGV

--- src/components/SubMenu/Agv/Produto.jsx
+++ src/components/SubMenu/Agv/Produto.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuAGV">
+            <img src="https://i.ibb.co/ScXr77k/agv.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
             
+            </div>
         </div>
     )
 }
