9aca0aa8b53948fc231e69fb3819800c5ecb418e
santiagoidu <sr_santiago@icloud.com>
2023-10-05 12:30:13 -0300

    feat: Sub Menu Helt

--- src/components/SubMenu/Helt/Produto.jsx
+++ src/components/SubMenu/Helt/Produto.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuHelt">
+            <img src="https://i.ibb.co/YtyrZc8/helt.png" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Jaquetas" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            
+            </div>
         </div>
     )
 }
