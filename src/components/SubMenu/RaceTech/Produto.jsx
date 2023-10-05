4a4ae6346d19dd9f8c84783088d07472fbf2c060
santiagoidu <sr_santiago@icloud.com>
2023-10-05 12:42:07 -0300

    feat: Sub Menu RaceTech

--- src/components/SubMenu/RaceTech/Produto.jsx
+++ src/components/SubMenu/RaceTech/Produto.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuRaceTech">
+            <img src="https://i.ibb.co/VDmGdk5/RaceTech.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
             
+            </div>
         </div>
     )
 }
