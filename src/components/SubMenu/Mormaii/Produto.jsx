e662b2f148a4dc5ac245368b4167b36caf1f439e
santiagoidu <sr_santiago@icloud.com>
2023-10-09 14:30:51 -0300

    feat: Sub Menu Mormaii

--- src/components/SubMenu/Mormaii/Produto.jsx
+++ src/components/SubMenu/Mormaii/Produto.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
+    return (
+        <div className="MenuMormaii">
+            <img src="https://i.ibb.co/8D546rf/mormaii.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            </div>
         </div>
     )
 }
