c9527bb9f37f2d07025b14f69d1ab963642df5d7
santiagoidu <sr_santiago@icloud.com>
2023-10-09 14:40:33 -0300

    feat: Sub Menu Fly

--- src/components/SubMenu/Fly/Produto.jsx
+++ src/components/SubMenu/Fly/Produto.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './Produto.css';
+import Categoria from './Categoria/Categoria'
 
 export default props => {
-    return(
-        <div className="Menu">
-            
+    return (
+        <div className="MenuFly">
+            <img src="https://i.ibb.co/yn7bh1M/Fly.jpg" className="logoImg" alt="logo" />
+            <div className="grid" >
+            <Categoria categoria="Capacete" />
+            <Categoria categoria="Reparo" />
+            <Categoria categoria="Viseira" />
+            </div>
         </div>
     )
 }
